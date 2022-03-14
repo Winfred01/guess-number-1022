229a6bbd9cc3aef039c5fb617b4dcc716f8242a2
JasonAtYork <jason666@my.yorku.ca>
2022-03-13 20:19:31 -0400

    testing

---
+++ What_Number_I_am_Thinking_of/app/src/test/java/com/example/what_number_i_am_thinking_of/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.example.what_number_i_am_thinking_of;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
