117f018527af3ac0d4d5d0f2b71d6d38720496f1
JasonAtYork <jason666@my.yorku.ca>
2022-03-26 17:14:43 -0400

    Pop up

--- What_Number_I_am_Thinking_of/app/src/main/java/com/example/what_number_i_am_thinking_of/MainActivity.java
+++ What_Number_I_am_Thinking_of/app/src/main/java/com/example/what_number_i_am_thinking_of/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.what_number_i_am_thinking_of;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,8 +19,31 @@ public class MainActivity extends AppCompatActivity {
 
     public void toGamingPage(View v){
         //switch to Gaming Page
-        Intent intent = new Intent(MainActivity.this, Gaming.class);
-        startActivity(intent);
+        final int[] checkedItem = {-1};
+        AlertDialog.Builder enterName = new AlertDialog.Builder(this);
+        enterName.setTitle("Please enter your name:");
+        final View customLayout = getLayoutInflater().inflate(R.layout.pop_up, null);
+        enterName.setView(customLayout);
+        enterName.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int which) {
+                AlertDialog.Builder selectDifficulty = new AlertDialog.Builder(MainActivity.this);
+                selectDifficulty.setTitle("Please select difficulty: ");
+                final String[] difficulty = new String[]{"Easy", "Normal", "Hard"};
+                selectDifficulty.setSingleChoiceItems(difficulty, checkedItem[0], new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        checkedItem[0] = i;
+                        Intent intent = new Intent(MainActivity.this, Gaming.class);
+                        startActivity(intent);
+                    }
+                });
+                AlertDialog diffDialog = selectDifficulty.create();
+                diffDialog.show();
+            }
+        });
+        AlertDialog dialog = enterName.create();
+        dialog.show();
     }
     public void toHistoryPage(View v){
         //switch to Match History
